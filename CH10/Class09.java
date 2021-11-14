1035951ff1e7c86803927a581bcfd43311fe0364
史丞育 <91858020+ChengYu-Shih@users.noreply.github.com>
2021-11-14 19:54:25 +0800

    Add files via upload

---
+++ CH10/Class09.java
@@ -0,0 +1,27 @@
+/*²Ä9ÃD
+ * 
+ */
+class Caaa1{
+	private int num;
+    public void display() {
+    	System.out.println("printed from Caaa class");
+    	
+    }
+	
+}
+class Cbbb1 extends Caaa1{
+	
+	public void display() {
+    	System.out.println("printed from Cbbb class");
+	}
+}
+public class Class09 {
+
+	public static void main(String[] args) {
+		Caaa1 a= new Caaa1();
+		Caaa1 b = new Cbbb1();
+		a.display();                //a
+		b.display();                //b
+	}
+
+}
