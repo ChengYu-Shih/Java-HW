494f73de10fe8aa25dc1b66fce2d019595195938
史丞育 <91858020+ChengYu-Shih@users.noreply.github.com>
2021-12-12 02:02:05 +0800

    Add files via upload

---
+++ ch11/class11.java
@@ -0,0 +1,56 @@
+interface Data {
+	public void showData();
+}
+
+interface Test {
+	public void showScore();
+
+	public double score();
+}
+
+class CStu implements Data, Test {
+	protected String id;                 // 學號
+	protected String name;               // 姓名
+	protected int mid;                   // 期中考成績
+	protected int finl;                  // 期末考成績
+	protected int common;                // 平時成績               
+
+	public CStu(String a, String b, int m, int f, int c) {
+		id = a;
+		name = b;
+		mid = m;
+		finl = f;
+		common = c;
+	}
+
+	public void show() {
+		showData();
+		showScore();
+	}
+
+	public void showScore() {
+
+		System.out.println("期中考成績:" + mid);
+		System.out.println("期末考成績:" + finl);
+		System.out.println("平時成績:" + common);
+		System.out.println("學期成績:" + score());
+	}
+	public void showData() {
+		System.out.println("學號:"+id);
+		System.out.println("姓名:"+name);
+	}
+	public double score(){
+		return (mid*0.3+finl*0.3+common*0.4);
+	
+	}
+}
+
+public class class11 {
+
+	public static void main(String[] args) {
+		CStu stu=new CStu("940001","Fiona",90,92,85);
+		stu.show();
+
+	}
+
+}
