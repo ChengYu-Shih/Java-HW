ab7b6cca097360832f9d6944456f2d1842860fac
史丞育 <91858020+ChengYu-Shih@users.noreply.github.com>
2022-01-09 15:20:10 +0800

    Add files via upload

---
+++ ch15/class12.java
@@ -0,0 +1,47 @@
+/*²Ä12ÃD
+ * 
+ */
+class CTest4 implements Runnable {
+	private String id;
+
+	public CTest4(String str) {
+		id = str;
+	}
+
+	public void run() {
+		for (int i = 1; i <= 5; i++) {
+			try {
+				Thread.sleep((int) (100 * Math.random()));
+			} catch (InterruptedException e) {
+			}
+			System.out.println(id + " " + i);
+		}
+	}
+}
+
+public class class12 {
+
+	public static void main(String[] args) {
+		CTest4 hi = new CTest4("Hello");
+		CTest4 bye = new CTest4("Good bye");
+		CTest4 morning = new CTest4("Good morning");
+		CTest4 night = new CTest4("Good night");
+		Thread t1 = new Thread(hi);
+		Thread t2 = new Thread(bye);
+		Thread t3 = new Thread(morning);
+		Thread t4 = new Thread(night);
+		t1.start();
+		try {
+			t1.join();
+			t3.start();
+			t3.join();
+			t4.start();
+			t4.join();
+			t2.start();
+			t2.join();
+		} catch (InterruptedException e) {
+		}
+		System.out.println("All Threads are finished");
+	}
+
+}
